fa682683289dfadcb2f2c93452ebba93f707eb66
Jon Reed <mr.is@outlook.com>
2015-04-24 09:55:14 -0400

    Migrated attributes and added some more attrib..

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,11 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
